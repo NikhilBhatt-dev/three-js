3071014f5fc1b0fc49993d9288ae1adc40ac0179
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-02-14 14:02:19 +0530

    first commit

---
+++ Main.js
@@ -0,0 +1,28 @@
+import *  as THREE from "three"
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75,innerWidth/innerHeight,0.1, 1000)
+scene.add(camera)
+
+
+ /* MESH =>{Shape & Material} */
+
+ const cubeGeometry = new THREE.BoxGeometry(1,1,1)
+ const cubeMaterial = new THREE.MeshStandardMaterial({
+    color: 0x00FF0
+ })
+
+ const cube =  new THREE.Mesh(cubeGeometry, cubeMaterial)
+ scene.add(cube)
+
+
+ const light = new THREE.DirectionalLight(0xFFFFFF,1)
+ scene.add(light)
+
+
+            /*  Renderer*/
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(innerWidth,innerHeight);
+
+document.body.appendChild(renderer.domElement);
